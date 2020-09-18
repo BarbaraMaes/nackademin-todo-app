94ce102882904387f1b0d948e4f53ddcb5e10b23
BarbaraMaes <maesbarbara92@gmail.com>
2020-09-18 10:37:21 +0200

    changed start script

--- start.js
+++ start.js
@@ -2,11 +2,10 @@ const app = require("./app");
 const Database = require("./database/database");
 require('dotenv').config()
 
-Database.connect().then(console.log("connected to db")).catch(err => console.log(err));
+Database.connect().then(() => {
+    app.listen(process.env.PORT || 8080, () => console.log("it's running"))
+}).catch(err => console.log(err));
 
-app.listen(process.env.PORT || 8080, () => {
+/*app.listen(process.env.PORT || 8080, () => {
     console.log(`server started on port ${process.env.PORT}`);
-})
-
-/*    "build": "cd backend nodemon start.js && cd frontend react-scripts start",
-    "start": "cd backend nodemon start.js"*/
+})*/
