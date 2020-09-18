549ef133e5a442f101b4bc06ae65e14b606fa5d6
BarbaraMaes <maesbarbara92@gmail.com>
2020-09-18 11:14:42 +0200

    changed homepage in package.json

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ if (process.env.ENV === 'production') {
 
     // Express serve up index.html file if it doesn't recognize route
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
     });
 }
 
