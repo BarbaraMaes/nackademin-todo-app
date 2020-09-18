21b2bee664dbc1ad31abbcc72a37ae7d0dd92049
BarbaraMaes <maesbarbara92@gmail.com>
2020-09-18 10:59:55 +0200

    added build folder for frontend

---
+++ build/precache-manifest.3b0b7326e1beeddba37b9ed18bfa31d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c1f934df2374e08a945891e424335f2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0bc9b156df70f2a791ec",
+    "url": "./static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e87beab064bb8708a442",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0bc9b156df70f2a791ec",
+    "url": "./static/js/2.96a9bc05.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.96a9bc05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e87beab064bb8708a442",
+    "url": "./static/js/main.3de5b857.chunk.js"
+  },
+  {
+    "revision": "6107ec8d66d05a23447d",
+    "url": "./static/js/runtime-main.1a950eca.js"
+  }
+]);
